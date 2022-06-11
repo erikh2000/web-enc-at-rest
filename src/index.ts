dba24d8557e01722ea356fa01d749f6e444b3393
erikh2000 <erikrhermansen@Eriks-iMac-2.local>
2022-06-11 15:10:30 -0700

    Before removing prettier.

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const sum = (a: number, b: number): number => a + b
+export const sub = (a: number, b: number): number => a - b
+export const div = (a: number, b: number): number => a / b
+export const mult = (a: number, b: number): number => a * b
